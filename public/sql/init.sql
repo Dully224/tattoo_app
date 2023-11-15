00544bd37ff014d719c01ea98c6aab489b1b8f13
DoubleDully <chudsonmusic@gmail.com>
2023-11-15 11:48:56 -0500

    format changes

--- public/sql/init.sql
+++ public/sql/init.sql
@@ -1,16 +1,17 @@
 /* login to mariadb using: sudo mysql */
 /* source this script using: source init.sql */
 /* source your reset file: source reset-data.sql */
-
 DROP DATABASE IF EXISTS tattoodb;
+
 CREATE DATABASE 'tattoodb';
 
-DROP USER IF EXISTS 'dully'@localhost;
-CREATE USER 'dully'@localhost IDENTIFIED BY '@Turtle122302';
+DROP USER IF EXISTS 'dully' @localhost;
+
+CREATE USER 'dully' @localhost IDENTIFIED BY '@Turtle122302';
 
 /* SELECT User FROM mysql.user WHERE User = 'week6user'; */
+GRANT ALL PRIVILEGES ON 'tattoodb'.* TO 'dully' @localhost IDENTIFIED BY '@Turtle122302';
 
-GRANT ALL PRIVILEGES ON 'tattoodb'.* TO 'dully'@localhost IDENTIFIED BY '@Turtle122302';
 FLUSH PRIVILEGES;
 
-SHOW GRANTS FOR 'dully'@localhost;
+SHOW GRANTS FOR 'dully' @localhost;
